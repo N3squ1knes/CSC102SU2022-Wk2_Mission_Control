6fbf40310da6a564fb027dd40cee2675055dddaf
Nestor Hernandez <97473410+N3squ1knes@users.noreply.github.com>
2022-05-19 12:23:29 -0700

    Add files via upload

---
+++ Code.js
@@ -0,0 +1,55 @@
+function blastoffTimerV1(){
+    console.log("blastoffTimerV1() Started");
+    var countdownTimer = 50;
+    document.getElementById("countdownDisplay").innerHTML = countdownTimer;
+    setTimeout(function(){
+        countdownTimer = countdownTimer - 5;
+        document.getElementById("countdownDisplay").innerHTML = countdownTimer;
+
+    }, 5000)
+    setTimeout(function(){
+        countdownTimer = countdownTimer - 5;
+        document.getElementById("countdownDisplay").innerHTML = countdownTimer;
+
+    }, 10000)
+    setTimeout(function(){
+        countdownTimer = countdownTimer - 5;
+        document.getElementById("countdownDisplay").innerHTML = countdownTimer;
+
+    }, 15000)
+    setTimeout(function(){
+        countdownTimer = countdownTimer - 5;
+        document.getElementById("countdownDisplay").innerHTML = countdownTimer;
+
+    }, 20000)
+    setTimeout(function(){
+        countdownTimer = countdownTimer - 5;
+        document.getElementById("countdownDisplay").innerHTML = countdownTimer;
+
+    }, 25000)
+    setTimeout(function(){
+        countdownTimer = countdownTimer - 5;
+        document.getElementById("countdownDisplay").innerHTML = countdownTimer;
+
+    }, 30000)
+    setTimeout(function(){
+        countdownTimer = countdownTimer - 5;
+        document.getElementById("countdownDisplay").innerHTML = countdownTimer;
+
+    }, 35000)
+    setTimeout(function(){
+        countdownTimer = countdownTimer - 5;
+        document.getElementById("countdownDisplay").innerHTML = countdownTimer;
+
+    }, 40000)
+    setTimeout(function(){
+        countdownTimer = countdownTimer - 5;
+        document.getElementById("countdownDisplay").innerHTML = countdownTimer;
+
+    }, 45000)
+    setTimeout(function(){
+        countdownTimer = countdownTimer - 5;
+        document.getElementById("countdownDisplay").innerHTML = "Blast off!!";
+
+    }, 50000)
+}
